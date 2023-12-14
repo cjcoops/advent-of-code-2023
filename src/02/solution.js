7ab34b71818ca4fd9b613a184e6e05b881e19400
Chris Cooper <cjcooper252@gmail.com>
2023-12-14 14:43:32 -0800

    day 2 part 1

--- src/02/solution.js
+++ src/02/solution.js
@@ -5,4 +5,42 @@ const text = await file.text();
 
 const lines = text.split("\n");
 
-console.log(lines);
+const CONFIGURATION = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+let result = 0;
+
+lines.forEach((line, index) => {
+  const tooManyReds = [...line.matchAll(new RegExp(/(\d+) red/, "g"))].some(
+    (match) => parseInt(match[1], 10) > CONFIGURATION.red
+  );
+
+  if (tooManyReds) {
+    return;
+  }
+
+  const tooManyBlue = [...line.matchAll(new RegExp(/(\d+) blue/, "g"))].some(
+    (match) => parseInt(match[1], 10) > CONFIGURATION.blue
+  );
+
+  if (tooManyBlue) {
+    return;
+  }
+
+  const tooManyGreen = [...line.matchAll(new RegExp(/(\d+) green/, "g"))].some(
+    (match) => parseInt(match[1], 10) > CONFIGURATION.green
+  );
+
+  if (tooManyGreen) {
+    return;
+  }
+
+  const gameId = index + 1;
+
+  result += parseInt(gameId, 10);
+});
+
+console.log(result);
