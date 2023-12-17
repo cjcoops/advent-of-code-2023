7031755b73010f9a9089e699ab4c701fac5f5199
Chris Cooper <cjcooper252@gmail.com>
2023-12-16 19:46:42 -0800

    init part 2 function

--- src/03/solution.js
+++ src/03/solution.js
@@ -39,4 +39,38 @@ function solvePartOne() {
   return result;
 }
 
-console.log(solvePartOne());
+function solvePartTwo() {
+  function isSymbol(value) {
+    return isNaN(value) && value !== ".";
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < lines.length; i++) {
+    const numbers = lines[i];
+    for (const match of numbers.matchAll(/\d+/g)) {
+      for (let j = match.index; j < match.index + match[0].length; j++) {
+        const surrounding = [
+          (lines[i - 1] ?? "")[j - 1] ?? ".",
+          (lines[i - 1] ?? "")[j] ?? ".",
+          (lines[i - 1] ?? "")[j + 1] ?? ".",
+          (lines[i] ?? "")[j - 1] ?? ".",
+          (lines[i] ?? "")[j] ?? ".",
+          (lines[i] ?? "")[j + 1] ?? ".",
+          (lines[i + 1] ?? "")[j - 1] ?? ".",
+          (lines[i + 1] ?? "")[j] ?? ".",
+          (lines[i + 1] ?? "")[j + 1] ?? ".",
+        ];
+
+        if (surrounding.some((x) => isSymbol(x))) {
+          result += parseInt(match[0]);
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(solvePartTwo());
