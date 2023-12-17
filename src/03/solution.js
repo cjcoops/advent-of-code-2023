d51c1cbb8b2bf77e7b5008cbfd282872f1981760
Chris Cooper <cjcooper252@gmail.com>
2023-12-16 19:42:53 -0800

    add function for part 1

--- src/03/solution.js
+++ src/03/solution.js
@@ -5,34 +5,38 @@ const text = await file.text();
 
 const lines = text.split("\n");
 
-function isSymbol(value) {
-  return isNaN(value) && value !== ".";
-}
+function solvePartOne() {
+  function isSymbol(value) {
+    return isNaN(value) && value !== ".";
+  }
 
-let result = 0;
+  let result = 0;
 
-for (let i = 0; i < lines.length; i++) {
-  const numbers = lines[i];
-  for (const match of numbers.matchAll(/\d+/g)) {
-    for (let j = match.index; j < match.index + match[0].length; j++) {
-      const surrounding = [
-        (lines[i - 1] ?? "")[j - 1] ?? ".",
-        (lines[i - 1] ?? "")[j] ?? ".",
-        (lines[i - 1] ?? "")[j + 1] ?? ".",
-        (lines[i] ?? "")[j - 1] ?? ".",
-        (lines[i] ?? "")[j] ?? ".",
-        (lines[i] ?? "")[j + 1] ?? ".",
-        (lines[i + 1] ?? "")[j - 1] ?? ".",
-        (lines[i + 1] ?? "")[j] ?? ".",
-        (lines[i + 1] ?? "")[j + 1] ?? ".",
-      ];
+  for (let i = 0; i < lines.length; i++) {
+    const numbers = lines[i];
+    for (const match of numbers.matchAll(/\d+/g)) {
+      for (let j = match.index; j < match.index + match[0].length; j++) {
+        const surrounding = [
+          (lines[i - 1] ?? "")[j - 1] ?? ".",
+          (lines[i - 1] ?? "")[j] ?? ".",
+          (lines[i - 1] ?? "")[j + 1] ?? ".",
+          (lines[i] ?? "")[j - 1] ?? ".",
+          (lines[i] ?? "")[j] ?? ".",
+          (lines[i] ?? "")[j + 1] ?? ".",
+          (lines[i + 1] ?? "")[j - 1] ?? ".",
+          (lines[i + 1] ?? "")[j] ?? ".",
+          (lines[i + 1] ?? "")[j + 1] ?? ".",
+        ];
 
-      if (surrounding.some((x) => isSymbol(x))) {
-        result += parseInt(match[0]);
-        break;
+        if (surrounding.some((x) => isSymbol(x))) {
+          result += parseInt(match[0]);
+          break;
+        }
       }
     }
   }
+
+  return result;
 }
 
-console.log(result);
+console.log(solvePartOne());
