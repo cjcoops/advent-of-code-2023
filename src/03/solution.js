b916f5cb79c4952d478e5cee03858bbd756af337
Chris Cooper <cjcooper252@gmail.com>
2023-12-17 19:41:21 -0800

    day3 part 2 wip

--- src/03/solution.js
+++ src/03/solution.js
@@ -1,4 +1,4 @@
-const path = "/input.txt";
+const path = "/example.txt";
 const file = Bun.file(import.meta.dir + path);
 
 const text = await file.text();
@@ -40,11 +40,8 @@ function solvePartOne() {
 }
 
 function solvePartTwo() {
-  function isSymbol(value) {
-    return isNaN(value) && value !== ".";
-  }
-
   let result = 0;
+  const maybeGears = new Map();
 
   for (let i = 0; i < lines.length; i++) {
     const numbers = lines[i];
@@ -62,13 +59,40 @@ function solvePartTwo() {
           (lines[i + 1] ?? "")[j + 1] ?? ".",
         ];
 
-        if (surrounding.some((x) => isSymbol(x))) {
-          result += parseInt(match[0]);
-          break;
-        }
+        const surroundingIndices = [
+          `${i - 1},${j - 1}`,
+          `${i - 1},${j}`,
+          `${i - 1},${j + 1}`,
+          `${i},${j - 1}`,
+          `${i},${j}`,
+          `${i},${j + 1}`,
+          `${i + 1},${j - 1}`,
+          `${i + 1},${j}`,
+          `${i + 1},${j + 1}`,
+        ];
+
+        surrounding.forEach((x, index) => {
+          if (x === "*") {
+            const starIndex = surroundingIndices[index];
+            console.log(match[0], surroundingIndices[index]);
+
+            const prev = maybeGears.get(starIndex);
+
+            console.log(prev);
+
+            if (prev) {
+              console.log(prev);
+              prev.add(match[0]);
+            } else {
+              maybeGears.set(starIndex, new Set([match[0]]));
+            }
+          }
+        });
       }
     }
   }
+
+  console.log(maybeGears);
 
   return result;
 }
