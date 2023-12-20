ea818e70539993d66abb992119f7c53a8aa7ce8d
Chris Cooper <cjcooper252@gmail.com>
2023-12-19 17:50:04 -0800

    day 4 pt 2 wip

--- src/04/solution.js
+++ src/04/solution.js
@@ -1,4 +1,4 @@
-const path = "/input.txt";
+const path = "/example.txt";
 const file = Bun.file(import.meta.dir + path);
 
 const text = await file.text();
@@ -34,7 +34,48 @@ function solvePartOne() {
   return result;
 }
 
-function solvePartTwo() {}
+function solvePartTwo() {
+  const numberOfWinnersByCard = new Map();
+  const numberOfWinnersArray = [];
+
+  cards.forEach((card) => {
+    const [cardAndWinners, myNumbers] = card.split("|");
+    const [cardNumber, winners] = cardAndWinners.split(":");
+
+    let numberOfWinners = 0;
+
+    splitString(myNumbers).forEach((number) => {
+      if (winners.includes(number)) {
+        numberOfWinners += 1;
+      }
+    });
+
+    numberOfWinnersByCard.set(cardNumber, numberOfWinners ?? 0);
+    numberOfWinnersArray.push(numberOfWinners);
+  });
+
+  console.log(numberOfWinnersByCard);
+  console.log(numberOfWinnersArray);
+  const numberOfCardsArray = new Array(cards.length).fill(1);
+
+  let result = 0;
+
+  // this needs to be recursive
+  numberOfWinnersArray.forEach((cardScore, cardIndex) => {
+    for (
+      let index = cardIndex + 1;
+      index < cardIndex + cardScore + 1;
+      index++
+    ) {
+      console.log(index);
+      numberOfCardsArray[index] += 1;
+    }
+  });
+
+  console.log(numberOfCardsArray);
+
+  return result;
+}
 
 console.log("Part 1: " + solvePartOne());
 console.log("Part 2: " + solvePartTwo());
