1052f7e7b1c776d82c8e3dae6db181a82a0a6b91
Chris Cooper <cjcooper252@gmail.com>
2024-11-13 15:30:58 -0800

    refactor 5

--- src/05/solution.js
+++ src/05/solution.js
@@ -3,44 +3,53 @@ const file = Bun.file(import.meta.dir + path);
 
 const text = await file.text();
 
-function solvePartOne() {
+function parseInput(text) {
   const sections = text.split(/\n\s*\n/);
   const [seedsLine, ...mapStrings] = sections;
 
-  function parseMap(inputString) {
-    const ranges = inputString.split("\n").map((rule) => {
-      const [destStart, sourceStart, length] = rule
-        .split(" ")
-        .map((value) => parseInt(value, 10));
-      return {
-        sourceStart,
-        sourceEnd: sourceStart + length - 1,
-        offset: destStart - sourceStart,
-      };
-    });
-
-    return ranges.sort((a, b) => a.sourceStart - b.sourceStart);
-  }
-
-  const mapResults = mapStrings.map((mapString) => {
-    return parseMap(mapString.split(":\n")[1]);
-  });
-
-  function getLocationBySeed(seedNumber) {
-    return mapResults.reduce((source, ranges) => {
-      const range = ranges.find(
-        (r) => source >= r.sourceStart && source <= r.sourceEnd
-      );
-      return range ? source + range.offset : source;
-    }, seedNumber);
-  }
-
-  const locationsBySeed = seedsLine
+  const seeds = seedsLine
     .split(": ")[1]
     .split(" ")
-    .map((seedNumber) => getLocationBySeed(parseInt(seedNumber, 10)));
+    .map((num) => parseInt(num, 10));
 
-  return Math.min(...locationsBySeed);
+  const maps = mapStrings.map((mapString) =>
+    parseMap(mapString.split(":\n")[1])
+  );
+
+  return { seeds, maps };
+}
+
+// Parse map section into ranges and sort ranges
+function parseMap(inputString) {
+  const ranges = inputString.split("\n").map((rule) => {
+    const [destStart, sourceStart, length] = rule
+      .split(" ")
+      .map((value) => parseInt(value, 10));
+
+    return {
+      sourceStart,
+      sourceEnd: sourceStart + length - 1,
+      offset: destStart - sourceStart,
+    };
+  });
+
+  return ranges.sort((a, b) => a.sourceStart - b.sourceStart);
+}
+
+// Map from a source through a series of ranges
+function mapThroughRanges(sourceNumber, maps) {
+  return maps.reduce((source, ranges) => {
+    const range = ranges.find(
+      (r) => source >= r.sourceStart && source <= r.sourceEnd
+    );
+    return range ? source + range.offset : source;
+  }, sourceNumber);
+}
+
+function solvePartOne() {
+  const { seeds, maps } = parseInput(text);
+  const locations = seeds.map((seed) => mapThroughRanges(seed, maps));
+  return Math.min(...locations);
 }
 
 function solvePartTwo() {}
